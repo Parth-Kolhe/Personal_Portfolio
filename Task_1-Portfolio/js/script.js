c1bfbaea94b16b198bf0ac43f5cfe1d91031a42a
Parth-Kolhe <141760688+Parth-Kolhe@users.noreply.github.com>
2023-08-10 11:00:59 +0530

    File Added

---
+++ Task_1-Portfolio/js/script.js
@@ -0,0 +1,9 @@
+/* Typing animation */
+var typed = new Typed(".typing", {
+    strings: ["","Web Developer", "Web Designer", "Cyber Security Devoted."],
+    typeSpeed: 100,
+    backSpeed: 60,  
+    loop: true
+    
+
+});
